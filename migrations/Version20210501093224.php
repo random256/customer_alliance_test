881761283019af7ee39d381cd822ccc9c9626f10
Roman Khalikov <rkhalikov@justaddmusic.net>
2021-05-01 11:33:30 +0200

    added entities and migrations

---
+++ migrations/Version20210501093224.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210501093224 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE hotel (id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE review (id VARCHAR(255) NOT NULL, hotel_id VARCHAR(255) NOT NULL, score INT NOT NULL, comment VARCHAR(255) NOT NULL, created_date DATETIME NOT NULL, INDEX IDX_794381C63243BB18 (hotel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C63243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C63243BB18');
+        $this->addSql('DROP TABLE hotel');
+        $this->addSql('DROP TABLE review');
+    }
+}
---
+++ src/Entity/Review.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReviewRepository::class)
+ */
+class Review
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="string")
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $comment;
+
+    /**
+     * @var Hotel
+     * @ORM\ManyToOne(targetEntity="Hotel")
+     * @ORM\JoinColumn(name="hotel_id", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     */
+    private $hotel;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->created_date;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $created_date): self
+    {
+        $this->created_date = $created_date;
+
+        return $this;
+    }
+
+    public function getHotel(): ?Hotel
+    {
+        return $this->hotel;
+    }
+
+    public function setHotel(Hotel $hotel): self
+    {
+        $this->hotel = $hotel;
+
+        return $this;
+    }
+}
